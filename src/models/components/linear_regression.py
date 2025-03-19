3406a8b27ee347bff22795d64da241a2fe145171
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2025-03-19 12:44:28 -0500

    linear_regression.TrialwiseLinearRegression: fixup to simplifying generative model
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- src/models/components/linear_regression.py
+++ src/models/components/linear_regression.py
@@ -7,7 +7,7 @@ from torch import nn
 from . import pyro as base
 
 class TrialwiseLinearRegression(base.PyroModel):
-    def __init__(self, ablations=[], hidden_dims=128, num_regressors=7,
+    def __init__(self, ablations=[], hidden_dims=128, num_regressors=4,
                  num_stimuli=4):
         super().__init__()
         self._ablations = ablations
