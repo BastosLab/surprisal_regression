498f55a5cbeb2cde5dbcd5425f0c3db8821595d6
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2025-03-30 17:13:22 -0500

    linear_regression.TrialwiseLinearRegression: neural proposals everywhere
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- src/models/components/linear_regression.py
+++ src/models/components/linear_regression.py
@@ -13,48 +13,44 @@ class TrialwiseLinearRegression(base.PyroModel):
         self._ablations = ablations
         self._num_regressors = num_regressors
         self._num_stimuli = num_stimuli
+        self._data_dim = self._num_stimuli * (num_regressors -\
+                                              len(self.ablations) + 1)
 
         if "repetition" not in self.ablations:
-            self.repetition_q_concentration = pnn.PyroParam(
-                torch.ones(1), constraint=dist.constraints.positive
-            )
-            self.repetition_q_rate = pnn.PyroParam(
-                torch.ones(1), constraint=dist.constraints.positive
-            )
             self.register_buffer("adaptation_concentration", torch.ones(1))
             self.register_buffer("adaptation_rate", torch.ones(1))
+            self.adaptation_q = nn.Sequential(
+                nn.Linear(self._data_dim, hidden_dims),
+                nn.SiLU(),
+                nn.Linear(hidden_dims, 2), nn.Softplus()
+            )
 
-        self.angle_q_alpha = pnn.PyroParam(torch.ones(2),
-                                           constraint=dist.constraints.simplex)
-        self.register_buffer("angle_p_alpha", torch.ones(2))
-
+        self.register_buffer("baseline_p_loc", torch.zeros(1))
+        self.register_buffer("baseline_p_log_scale", torch.zeros(1))
         self.baseline_q = nn.Sequential(
-            nn.Linear(num_stimuli * (num_regressors + 1), hidden_dims),
+            nn.Linear(self._data_dim, hidden_dims),
             nn.SiLU(),
             nn.Linear(hidden_dims, 2)
         )
-        self.register_buffer("baseline_p_loc", torch.zeros(1))
-        self.register_buffer("baseline_p_log_scale", torch.zeros(1))
 
         if "selectivity" not in self.ablations:
-            self.selectivity_q_concentration = pnn.PyroParam(
-                torch.ones(1), constraint=dist.constraints.positive
-            )
-            self.selectivity_q_rate = pnn.PyroParam(
-                torch.ones(1), constraint=dist.constraints.positive
-            )
+            self.register_buffer("angle_p_alpha", torch.ones(2))
             self.register_buffer("selectivity_p_concentration", torch.ones(1))
             self.register_buffer("selectivity_p_rate", torch.ones(1))
+            self.selectivity_q = nn.Sequential(
+                nn.Linear(self._data_dim, hidden_dims),
+                nn.SiLU(),
+                nn.Linear(hidden_dims, 4), nn.Softplus()
+            )
 
         if "surprise" not in self.ablations:
-            self.surprise_q_concentration = pnn.PyroParam(
-                torch.ones(4), constraint=dist.constraints.simplex
+            self.register_buffer("surprise_p_concentration", torch.ones(1))
+            self.register_buffer("surprise_p_rate", torch.ones(1))
+            self.surprise_q = nn.Sequential(
+                nn.Linear(self._data_dim, hidden_dims),
+                nn.SiLU(),
+                nn.Linear(hidden_dims, 2), nn.Softplus()
             )
-            self.surprise_q_rate = pnn.PyroParam(
-                torch.ones(4), constraint=dist.constraints.simplex
-            )
-            self.register_buffer("surprise_p_concentration", torch.ones(4))
-            self.register_buffer("surprise_p_rate", torch.ones(4))
 
     @property
     def ablations(self):
@@ -62,32 +58,33 @@ class TrialwiseLinearRegression(base.PyroModel):
 
     def guide(self, muae, regressors):
         B = muae.shape[0]
-        data = torch.cat((muae, regressors), dim=-1)
-        loc, log_scale = self.baseline_q(data.flatten(-2, -1)).unbind(dim=-1)
+        data = torch.cat((muae, self.trial_regressors(regressors)),
+                         dim=-1).flatten(-2, -1)
+        loc, log_scale = self.baseline_q(data).unbind(dim=-1)
         baseline = pyro.sample("baseline", dist.Normal(
             loc.unsqueeze(dim=-1), log_scale.exp().unsqueeze(dim=-1)
         ).to_event(1))
         P = baseline.shape[0]
 
         if "selectivity" not in self.ablations:
-            alpha = self.angle_alpha.expand(B, 2)
+            features = self.selectivity_q(data).expand(P, B, 4) + 1e-5
+            alpha = features[:, :, :2]
             orientation = pyro.sample("orientation", dist.Dirichlet(alpha))
 
-            concentration = self.selectivity_q_concentration.expand(B, 1)
-            rate = self.selectivity_q_rate.expand(B, 1)
+            concentration, rate = features[:, :, 2:3], features[:, :, 3:4]
             selectivity = orientation * pyro.sample(
                 "selectivity", dist.Gamma(concentration, rate).to_event(1)
             )
 
         if "repetition" not in self.ablations:
-            concentration = self.repetition_q_concentration.expand(B, 1)
-            rate = self.repetition_q_rate.expand(B, 1)
-            repetition_dist = dist.Gamma(concentration, rate).to_event(1)
-            repetition = pyro.sample("repetition", repetition_dist)
+            features = self.adaptation_q(data).expand(P, B, 2) + 1e-5
+            concentration, rate = features[:, :, 0:1], features[:, :, 1:2]
+            pyro.sample("adaptation",
+                        dist.Gamma(concentration, rate).to_event(1))
 
         if "surprise" not in self.ablations:
-            concentration = self.surprise_q_concentration.expand(B, 4)
-            rate = self.surprise_q_rate.expand(B, 4)
+            features = self.surprise_q(data).expand(P, B, 2) + 1e-5
+            concentration, rate = features[:, :, 0:1], features[:, :, 1:2]
             surprise = pyro.sample("surprise",
                                    dist.Gamma(concentration, rate).to_event(1))
 
@@ -95,6 +92,7 @@ class TrialwiseLinearRegression(base.PyroModel):
         # regressors[:, 0:1] = one-hot for orientation (0 -> 45, 1 -> 135)
         # regressors[:, 2] = stimulus repetition count up to current stimulus
         # regressors[:, 3:6] = surprisals
+        regressors = self.trial_regressors(regressors)
         B = regressors.shape[0]
         loc = self.baseline_p_loc.expand(B, 1)
         log_scale = self.baseline_p_log_scale.expand(B, 1)
@@ -105,10 +103,10 @@ class TrialwiseLinearRegression(base.PyroModel):
         if "repetition" in self.ablations:
             repetition = regressors.new_zeros(torch.Size((P, B, 1)))
         else:
-            concentration = self.repetition_p_concentration.expand(B, 1)
-            rate = self.repetition_p_rate.expand(B, 1)
-            repetition = pyro.sample(
-                "repetition", dist.Gamma(concentration, rate).to_event(1)
+            concentration = self.adaptation_concentration.expand(B, 1)
+            rate = self.adaptation_rate.expand(B, 1)
+            adaptation = pyro.sample(
+                "adaptation", dist.Gamma(concentration, rate).to_event(1)
             )
 
         if "selectivity" in self.ablations:
@@ -124,18 +122,18 @@ class TrialwiseLinearRegression(base.PyroModel):
             )
 
         if "surprise" in self.ablations:
-            surprise = regressors.new_zeros(torch.Size((P, B, 4)))
+            surprise = regressors.new_zeros(torch.Size((P, B, 1)))
         else:
-            concentration = self.surprise_p_concentration.expand(B, 4)
-            rate = self.surprise_p_rate.expand(B, 4)
+            concentration = self.surprise_p_concentration.expand(B, 1)
+            rate = self.surprise_p_rate.expand(B, 1)
             surprise = pyro.sample("surprise",
                                    dist.Gamma(concentration, rate).to_event(1))
 
         baseline = baseline.unsqueeze(-2).expand(*baseline.shape[:2], 4, 1)
 
-        coefficients = torch.cat((orientation * selectivity, -repetition,
-                                  surprise), dim=-1)
-        regressors = regressors.expand(coefficients.shape[0], *regressors.shape)
+        coefficients = torch.cat((orientation * selectivity, -adaptation,
+                                  surprise), dim=-1)[:,:,self.regressor_indices]
+        regressors = regressors.expand(P, *regressors.shape)
         coefficients = coefficients.unsqueeze(-2).expand(*regressors.shape)
         predictions = torch.linalg.vecdot(coefficients, regressors)
         predictions = predictions.unsqueeze(dim=-1) + baseline
@@ -143,3 +141,17 @@ class TrialwiseLinearRegression(base.PyroModel):
                     obs=muae.unsqueeze(dim=0) if muae is not None else None)
 
         return predictions
+
+    @property
+    def regressor_indices(self):
+        indices = []
+        if "selectivity" not in self.ablations:
+            indices += [0, 1]
+        if "repetition" not in self.ablations:
+            indices.append(2)
+        if "surprise" not in self.ablations:
+            indices.append(3)
+        return indices
+
+    def trial_regressors(self, regressors):
+        return regressors[:, :, self.regressor_indices]
