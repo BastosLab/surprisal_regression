791c9fd458e6f2ae05aaae22c20a0c1544cd52da
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2025-04-02 15:26:27 -0500

    train_all_mouse: script for executing a training run across mouse sessions
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

---
+++ train_all_mouse.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import glob
+import hdf5storage as mat
+import itertools
+import json
+import multiprocessing
+import os
+import re
+import subprocess
+from tqdm import tqdm
+
+ABLATIONS = ["repetition", "surprise"]
+ABLATION_SETTINGS = list(itertools.chain(
+    itertools.combinations(ABLATIONS, 0), itertools.combinations(ABLATIONS, 1),
+    itertools.combinations(ABLATIONS, 2)
+))
+AREAS_HIERARCHY = ['V1', 'LM', 'RL', 'AL', 'PM', 'AM']
+
+LOGDIR_RE = r"surprisal_regression/logs/train/runs/(.*)/checkpoints/"
+
+SESSION_DIR = '/mnt/data/epoched_mouse_passiveglo'
+SESSIONS = sorted(glob.glob(SESSION_DIR + "/*.mat"))
+TRAINING_SESSIONS = {}
+
+def session_areas(session):
+    raw = mat.loadmat(session, squeeze_me=True)['datastruct']
+    raw = dict(zip(raw.dtype.names, raw.item()))
+    possible_areas = [area.item() for area in raw['areas'][:, 0].tolist()]
+    timestamps = {
+            k: v for (k, v) in zip(possible_areas, raw['muae'][0, :].tolist())
+            if len(v)
+    }
+    del raw
+    for area in timestamps.keys():
+        if area in AREAS_HIERARCHY:
+            yield area
+
+def train_ablation(ablations):
+    ablation_spec = 'model.importance.ablations=%s' % str(list(ablations))
+    completed = subprocess.run(['python', 'src/train.py', 'data=mouse',
+                                session_spec, area_spec, ablation_spec],
+                               stderr=subprocess.PIPE,
+                               stdout=subprocess.DEVNULL)
+    m = re.search(LOGDIR_RE, completed.stderr.decode('utf-8'))
+    if m is not None:
+        m = m.groups()[0]
+    return m
+
+if __name__ == "__main__":
+    for session in tqdm(SESSIONS, desc='Sessions'):
+        session_spec = 'data.session_path=%s' % session
+        TRAINING_SESSIONS[session] = {}
+        for area in tqdm(session_areas(session), desc='Areas', leave=False):
+            area_spec = 'data.area=%s' % area
+            TRAINING_SESSIONS[session][area] = []
+            with multiprocessing.Pool(8) as pool:
+                ms = pool.map(train_ablation, ABLATION_SETTINGS)
+                for (ablations, m) in zip(ABLATION_SETTINGS, ms):
+                    TRAINING_SESSIONS[session][area].append((ablations, m))
+
+    with open("TRAINING_SESSIONS_MOUSE.json", "w") as f:
+        json.dump(TRAINING_SESSIONS, f, indent=4)
